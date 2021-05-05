eec08e056c8de20fdc07e58252301d571f33c0dd
vmidor <76964172+vmidor@users.noreply.github.com>
2021-05-05 17:09:03 -0400

    added zip

--- public/js/login.js
+++ public/js/login.js
@@ -27,13 +27,14 @@ const signupFormHandler = async (event) => {
 
   const first_name = document.querySelector('#first-name-signup').value.trim();
   const last_name = document.querySelector('#last-name-signup').value.trim();
+  const zipcode = document.querySelector('#zip-code').value.trim();
   const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
 
   if (first_name && last_name && email && password) {
     const response = await fetch('/api/users', {
       method: 'POST',
-      body: JSON.stringify({ first_name, last_name, email, password }),
+      body: JSON.stringify({ first_name, last_name, zipcode, email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
 
