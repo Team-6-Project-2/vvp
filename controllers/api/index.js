414fc2b40362eb49234860aa46832f0f64e91cb4
The Podz <stefanpodz@yahoo.com>
2021-05-07 20:32:17 -0400

    more cleanup

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,12 +2,12 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const vaxxRoutes = require('./vaxxRoutes');
 const emailRoutes = require('./emailRoutes');
-const covidRoutes = require('./covidRoutes');
+
 
 router.use('/users', userRoutes);
 router.use('/vaxxs', vaxxRoutes);
 router.use('/email', emailRoutes);
-router.use('/covid', covidRoutes);
+
 
 
 
